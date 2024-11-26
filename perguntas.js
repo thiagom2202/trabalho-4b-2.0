cd908ea183dda65c9e3aedfcafb22ef6ef524030
thiagom2202 <00001121658258sp@al.educacao.sp.gov.br>
2024-11-26 07:49:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'Por que aprender programar?',
+    'Mercado de trabalho esta exigindo pessoas com habilidade em linguaguem de programação, é um diferencial'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Nota Bimestre',
+    'Quando finaliza nosso bimestre?',
+    'As notas serão fechadas a partir de 27/11'
+)
